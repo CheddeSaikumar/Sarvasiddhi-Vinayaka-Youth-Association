8f145f1a78e537c8ee21e621827dec60f7ba4e6a
CheddeSaikumar <saikumar.c17@gmail.com>
2026-09-11 21:24:36 +0530

    git actions page effect

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sarvasiddhi-Vinayaka-Youth-Association/',
   plugins: [react()],
   resolve: {
     alias: {
