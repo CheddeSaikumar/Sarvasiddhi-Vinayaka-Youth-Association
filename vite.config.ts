e38a1bd9683301e7a467b77b8a0fcb9424452cc1
CheddeSaikumar <saikumar.c17@gmail.com>
2026-09-11 21:31:26 +0530

    Revert "git actions page effect"
    This reverts commit 8f145f1a78e537c8ee21e621827dec60f7ba4e6a.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Sarvasiddhi-Vinayaka-Youth-Association/',
   plugins: [react()],
   resolve: {
     alias: {
